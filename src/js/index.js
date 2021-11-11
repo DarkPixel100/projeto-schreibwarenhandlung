9722de8af747ca94ff3aa20da167145d8222a589
DarkPixel100 <diegofavila20@gmail.com>
2021-11-11 02:31:38 -0300

    Comentários, otimização e re-formatação

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 let senha = document.getElementById("senha");
 let olho = document.getElementById("mostrar");
 senha.submit;
+// Clique mostra a senha em texto plano
 olho.addEventListener('click', () => {
   if (senha.type === 'password') {
-    senha.type = 'text';
+    senha.type = 'text'; // Trocando o tipo do input
+    // Trocando a classe que define o ícone do FontAwesome
     olho.children[0].classList.add('fa-eye');
     olho.children[0].classList.remove('fa-eye-slash');
   } else {
