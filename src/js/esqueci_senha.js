1840323dfebd8ee54613ef0dfbb78a9e536a7d9b
ArthurBragaD <arthurbragadutra@gmail.com>
2021-11-11 19:17:30 -0300

    Criação de um js para esqueci minha senha

---
+++ src/js/esqueci_senha.js
@@ -0,0 +1,15 @@
+let troca = document.getElementById("trocar");
+let formato = "senha"
+troca.addEventListener('click', () => {
+    if (formato === "senha") {
+        document.getElementById("title").innerHTML = "Recuperacao de Usuário"
+        document.getElementById("trocar").innerHTML = "Nao sei minha senha"
+        document.getElementById("username").placeholder = "Seu Email"
+        formato = "usuario"
+    } else {
+        document.getElementById("title").innerHTML = "Recuperacao de senha"
+        document.getElementById("username").placeholder = "Nome de Usuário"
+        document.getElementById("trocar").innerHTML = "Nao sei meu usuario"
+        formato = "senha"
+    }
+});
